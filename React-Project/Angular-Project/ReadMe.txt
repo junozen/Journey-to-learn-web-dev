53ef68ec079801706e1d3b28d51da9e548b9f30c
Juno <115151434+Junozen@users.noreply.github.com>
2023-02-10 15:35:48 +0200

    Add files via upload

---
+++ React-Project/Angular-Project/ReadMe.txt
@@ -0,0 +1,3 @@
+A very small angular project to showcase some elements of the framework and familiarize myself with it.
+
+Stripped of node modules for space reasons, but feel free to inspect the code! 
---
+++ React-Project/Angular-Project/src/app/new-comp/new-comp.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-new-comp',
+  templateUrl: './new-comp.component.html',
+  styleUrls: ['./new-comp.component.css']
+})
+export class NewCompComponent implements OnInit {
+
+  @Input() featureSet: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
